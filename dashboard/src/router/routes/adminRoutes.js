85c90b25fa8f6579e5e442c8650d535db13825f3
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-11-27 23:21:43 +0100

    [FRONT] Orders Admin {OrderTable}

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -1,10 +1,16 @@
 import { lazy } from "react";
 const AdminDashboard = lazy(() => import("../../views/admin/AdminDashboard"))
+const Orders = lazy(() => import("../../views/admin/Orders"))
 
 export const adminRoutes = [
     {
         path : 'admin/dashboard',
         element : <AdminDashboard />,
+        ability : 'admin'
+    },
+    {
+        path : 'admin/dashboard/orders',
+        element : <Orders />,
         ability : 'admin'
     }
 ]
