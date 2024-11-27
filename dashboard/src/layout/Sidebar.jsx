e5eff4922db77ef8b17e55a6953acf38a09558af
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-11-27 16:31:15 +0100

    [BACK] Navigation {Sidebar,Logout}

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState} from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { getNavs } from '../navigation/index'
+import { BiLogInCircle } from 'react-icons/bi'
 
 const Sidebar = () => {
   const {pathname} = useLocation()
@@ -29,6 +30,12 @@ const Sidebar = () => {
                 </Link>
               </li>)
             }
+            <li>
+              <button className='text-[#d0d2d6] font-normal duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1'>
+                <span><BiLogInCircle /></span>
+                <span>Logout</span>
+              </button>
+            </li>
           </ul>
         </div>
       </div>
