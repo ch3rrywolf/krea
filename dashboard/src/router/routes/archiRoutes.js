fc7b614d02c5b58c874647583640b47beabfe030
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-12-01 23:30:32 +0100

    [FRONT] DiscountProduct Archi {DiscountProduct.jsx.}

--- dashboard/src/router/routes/archiRoutes.js
+++ dashboard/src/router/routes/archiRoutes.js
@@ -1,8 +1,10 @@
 import { lazy } from "react";
+
 const Home = lazy(() => import("../../views/Home"))
 const ArchiDashboard = lazy(() => import("../../views/archi/ArchiDashboard"))
 const AddProduct = lazy(() => import("../../views/archi/AddProduct"))
 const Products = lazy(() => import("../../views/archi/Products"))
+const DiscountProducts = lazy(() => import("../../views/archi/DiscountProducts"))
 
 export const archiRouters = [
     {
@@ -23,6 +25,11 @@ export const archiRouters = [
     {
         path : '/archi/dashboard/products',
         element : <Products />,
+        ability : ['archi']
+    },
+    {
+        path : '/archi/dashboard/discount-product',
+        element : <DiscountProducts />,
         ability : ['archi']
     }
 ]
