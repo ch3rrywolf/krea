2eb1fd3e6cec29f388ec5c19d77d95b54db9e6f2
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-12-05 11:18:08 +0100

    [BACK&FRONT] Admin AddCategory {addCategory}2/2

--- backend/controllers/dashboard/categoryController.js
+++ backend/controllers/dashboard/categoryController.js
@@ -1,6 +1,17 @@
+const categoryModel = require('../../models/categoryModel')
+const formidable = require('formidable')
 class categoryController {
     add_category = async (req, res) => {
-        console.log("gfg")
+        const form = new formidable.IncomingForm();
+        form.parse(req,async(err,fields,files)=>{
+            console.log(files)
+            console.log(fields)
+        })
+        
+    }
+
+    get_category = async (req, res) => {
+        console.log("fgf")
     }
 
 }
