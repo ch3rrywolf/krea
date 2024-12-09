e5cf1027ec2a688b511d40d009aea2db6bbe360c
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-12-09 19:11:39 +0100

    [FRONT] Client Espace Components{Header.}

--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -7,12 +7,15 @@ import { Link, useLocation } from 'react-router-dom'
 
 const Headers = () => {
     const {pathname} = useLocation()
-    const [showShidebar, setShowShidebar] = useState(false)
+    const [showShidebar, setShowShidebar] = useState(true)
+    const [categoryShow, setCategoryShow] = useState(true)
     const user = true
     console.log(pathname)
+
+    const categorys = ['Meubles','Décoration','Jardin','ParPièce','Bâtiment/Architecture']
   return (
     <div className='w-full bg-white'>
-            <div className='header-top bg-[#eeeeee] md-lg:hidden'>
+            <div className='header-top bg-[#D4BFAA] md-lg:hidden'>
                 <div className='w-[85%] lg:w-[90%] mx-auto'>
                     <div className='flex w-full justify-between items-center h-[50px] text-slate-500'>
                         <ul className='flex justify-start items-center gap-8'>
@@ -67,27 +70,58 @@ const Headers = () => {
                         <div className='flex justify-between md-lg:justify-center items-center flex-wrap pl-8'>
                             <ul className='flex justify-start items-start gap-8 text-sm font-bold uppercase md-lg:hidden'>
                                 <li>
-                                    <Link className={`p-2 block ${pathname === '/' ? 'text-[#7fad39]':'text-slate-600'}`}>Home</Link>
+                                    <Link className={`p-2 block ${pathname === '/' ? 'text-[#8A4D76]':'text-slate-600'}`}>Home</Link>
                                 </li>
                                 <li>
-                                    <Link className={`p-2 block ${pathname === '/shop' ? 'text-[#7fad39]':'text-slate-600'}`}>Shop</Link>
+                                    <Link className={`p-2 block ${pathname === '/shop' ? 'text-[#8A4D76]':'text-slate-600'}`}>Shop</Link>
                                 </li>
                                 <li>
-                                    <Link className={`p-2 block ${pathname === '/blog' ? 'text-[#7fad39]':'text-slate-600'}`}>Blog</Link>
+                                    <Link className={`p-2 block ${pathname === '/blog' ? 'text-[#8A4D76]':'text-slate-600'}`}>Blog</Link>
                                 </li>
                                 <li>
-                                    <Link className={`p-2 block ${pathname === '/about' ? 'text-[#7fad39]':'text-slate-600'}`}>About</Link>
+                                    <Link className={`p-2 block ${pathname === '/about' ? 'text-[#8A4D76]':'text-slate-600'}`}>About</Link>
                                 </li>
                                 <li>
-                                    <Link className={`p-2 block ${pathname === '/contact' ? 'text-[#7fad39]':'text-slate-600'}`}>Contact</Link>
+                                    <Link className={`p-2 block ${pathname === '/contact' ? 'text-[#8A4D76]':'text-slate-600'}`}>Contact</Link>
                                 </li>
                             </ul>
                         </div>
                         </div>
+                        
                     </div>
                 </div>
+                
             </div>
+            <div className='w-[85%] lg:w-[90%] mx-auto'>
+                <div className='flex w-full flex-wrap md-lg:gap-8'>
+                    <div className='w-3/12 md-lg:w-full'>
+                        <div className='bg-white relative'>
+                            <div onClick={()=>setCategoryShow(!categoryShow)} className='h-[50px] bg-violet-400 text-white flex justify-center md-lg:justify-between md-lg:px-6 items-center gap-3 font-bold text-md cursor-pointer'>
+                                <div className='flex justify-center items-center gap-3'>
+                                    <span><FaList/></span>
+                                    <span>All Category</span>
+                                </div>
+                                <span className='pt-1'><MdOutlineKeyboardArrowDown/></span>
+                            </div>
+                            <div className={`${categoryShow ? 'h-0' : 'h-[400px]'} overflow-hidden transition-all md-lg:relative duration-500 absolute z-[99999] bg-white w-full border-x`}>
+                                <ul className='py-2 text-slate-600 font-medium h-full overflow-auto'>
+                                    {
+                                        categorys.map((c, i) => {
+                                            return (
+                                                <li key={i} className='flex justify-start items-center gap-2 px-[24px] py-[6px]'>
+                                                    <Link className='text-sm block'>{c}</Link>
+                                                </li>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>   
         </div>
+        
   )
 }
 
