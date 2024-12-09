2d9412915580d530319eb5952404b85fa5607a71
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-12-09 22:34:18 +0100

    [FRONT] Client Espace Components{FeaturedProductsImages.}

--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -66,11 +66,11 @@ const Headers = () => {
             </div>
             <div className='w-white'>
                 <div className='w-[85%] lg:w-[90%] mx-auto'>
-                    <div className='h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap'>
+                    <div className='h-[260px] md-lg:h-[100px] flex justify-between items-center flex-wrap'>
                         <div className='md-lg:w-full w-3/12 md-lg:pt-4'>
                             <div className='flex justify-between items-center'>
                                 <Link to='/'>
-                                    <img src="http://localhost:3000/images/logo.png" alt="logo" />
+                                    <img src="http://localhost:3001/images/logo/logo2.png" alt="logo" />
                                 </Link>
                                 <div className='justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden' onClick={() => setShowShidebar(false)}>
                                     <span><FaList /></span>
@@ -119,7 +119,7 @@ const Headers = () => {
                 <div className={`w-[300px] z-[9999] transition-all duration-200 fixed  ${showShidebar ? '-left-[300px]' : 'left-0'} top-0 overflow-y-auto bg-white h-screen py-6 px-8`}>
                     <div className='flex justify-start flex-col gap-6'>
                         <Link to='/'>
-                            <img src="http://localhost:3000/images/logo.png" alt="logo" />
+                            <img src="http://localhost:3001/images/logo/logo2.png" alt="logo" />
                         </Link>
                         <div className='flex justify-star items-center gap-10'>
                             <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute'>
