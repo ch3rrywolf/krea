8fcfc0222d34b914e6c05852ea14cdd0a6e02586
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-12-09 21:41:02 +0100

    [FRONT] Client Espace Components{Banner}

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import Headers from '../components/Headers'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Heders from '../components/Headers'
+import Banner from '../components/Banner'
+import Categorys from '../components/Categorys'
 
 const Home = () => {
-  return (
-    <Headers />
-  )
+   
+    return (
+        <div className='w-full'>
+            <Heders />
+            <Banner />
+            <div className='my-4'>
+                <Categorys />
+            </div>
+            
+        </div>
+    )
 }
 
 export default Home
